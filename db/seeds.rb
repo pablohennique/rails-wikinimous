21270ddbb3f3e577e938870476f862eb5b7a5c3c
Pablo Hennique <phennique@gmail.com>
2023-05-23 16:57:46 -0400

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "cleaning databased"
+Article.destroy_all
+
+puts 'creating articles'
+Article.create(title: 'The meaning of life', content: "It's 42!")
+Article.create(title: 'The meaning of heaven', content: "It's 1!")
+Article.create(title: 'The meaning of hell', content: "It's 23!")
